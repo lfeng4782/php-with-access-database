2ae93226701455071afdd0cba29e796e529583a7
lfeng4782 <phyflx@hotmail.com>
2018-05-08 16:39:50 -0400

    add test

--- vendors.php
+++ vendors.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Assignment5</title>
+		<title>php practice</title>
 		<link rel="stylesheet" href="assignment5.css">    
 	</head>
 
